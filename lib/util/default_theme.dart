42923e82933ed89c8de836f1d063fcd03f52f12a
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-01-05 15:17:39 +1100

    format(DefaultTheme): formatted file whitespacing.

--- lib/util/default_theme.dart
+++ lib/util/default_theme.dart
@@ -3,29 +3,29 @@ import 'package:flutter/material.dart';
 class DefaultTheme {
   static ThemeData lightMode = ThemeData(
     colorScheme: ColorScheme(
-      brightness: Brightness.light, 
-      primary: Color(0xFF0a0a0a), 
-      onPrimary: Color(0xFF252525), 
-      secondary: Colors.white, 
-      onSecondary: Color(0xFFE7E7E7), 
-      error: Colors.red, 
-      onError: Colors.yellow, 
-      surface: Color(0xFFf5f5f5), 
-      onSurface: Colors.white
-    )
+      brightness: Brightness.light,
+      primary: Color(0xFF0a0a0a),
+      onPrimary: Color(0xFF252525),
+      secondary: Colors.white,
+      onSecondary: Color(0xFFE7E7E7),
+      error: Colors.red,
+      onError: Colors.yellow,
+      surface: Color(0xFFf5f5f5),
+      onSurface: Colors.white,
+    ),
   );
 
   static ThemeData darkMode = ThemeData(
     colorScheme: ColorScheme(
-      brightness: Brightness.dark, 
-      primary: Color(0xFFf0f0f0), 
-      onPrimary: Color(0xFF1f1f1f), 
-      secondary: Color(0xFF1a1a1a), 
-      onSecondary: Color(0xFF1f1f1f), 
-      error: Colors.red, 
-      onError: Colors.yellow, 
-      surface: Color(0xFF1f1f1f), 
-      onSurface: Colors.black
-    )
+      brightness: Brightness.dark,
+      primary: Color(0xFFf0f0f0),
+      onPrimary: Color(0xFF1f1f1f),
+      secondary: Color(0xFF1a1a1a),
+      onSecondary: Color(0xFF1f1f1f),
+      error: Colors.red,
+      onError: Colors.yellow,
+      surface: Color(0xFF1f1f1f),
+      onSurface: Colors.black,
+    ),
   );
 }
