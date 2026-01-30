26936c1d32d46fdef002517d25de30fa7c6f4bdc
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-01-30 21:38:38 +1100

    feat(MemoryCard): added SlidableWidget to class.

--- lib/widgets/memory_card.dart
+++ lib/widgets/memory_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:memorystack/models/memory.dart';
 
 class MemoryCard extends StatelessWidget {
@@ -9,66 +10,87 @@ class MemoryCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 55, left: 15, top: 10, bottom: 10),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.secondary,
-          borderRadius: BorderRadius.circular(7),
-        ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(7),
+        child: Slidable(
+          key: ValueKey(memory.id),
+          endActionPane: ActionPane(
+            motion: ScrollMotion(),
+            children: [
+              SlidableAction(
+                onPressed: (context) {},
+                icon: Icons.delete,
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+              ),
 
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              SlidableAction(
+                onPressed: (context) {},
+                icon: Icons.edit,
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+              ),
+            ],
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+
+            child: Column(
               children: [
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 15, top: 10),
-                      child: Text(
-                        memory.name,
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.primary,
-                          fontWeight: FontWeight.w500,
-                          letterSpacing: 0,
-                          overflow: TextOverflow.ellipsis,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15, top: 10),
+                          child: Text(
+                            memory.name,
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 0,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
+
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 15, top: 10),
+                        child: Text(
+                          "20/02/2026",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                            fontSize: 8,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
 
                 Align(
-                  alignment: Alignment.topRight,
+                  alignment: Alignment.centerLeft,
                   child: Padding(
-                    padding: const EdgeInsets.only(right: 15, top: 10),
+                    padding: const EdgeInsets.only(left: 16, right: 16),
                     child: Text(
-                      "20/02/2026",
+                      memory.description,
                       style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                        fontSize: 8,
+                        color: Theme.of(context).colorScheme.primary,
+                        fontSize: 10,
+                        letterSpacing: 0,
                       ),
                     ),
                   ),
                 ),
               ],
             ),
-
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                child: Text(
-                  memory.description,
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontSize: 10,
-                    letterSpacing: 0,
-                  ),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
